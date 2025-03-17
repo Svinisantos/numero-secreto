9132a6e157cfc5140585603ecde451742aa0bb0f
Vinicius Santos <svinisantos.silva@gmail.com>
2025-03-17 08:19:46 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -16,7 +16,7 @@ function exibirTexto(tag, texto) {
 
 function exibirMensagemInicial(){
     exibirTexto('h1', 'Jogo do número secreto');
-    exibirTexto('p', 'Escolha um número entre 1 e 10');
+    exibirTexto('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial()
